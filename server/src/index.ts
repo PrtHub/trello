89392b573c5b0d781e58b4de1975720139a368bc
Pritam Ghosh <pri10145@gmail.com>
2024-07-29 17:00:32 +0530

    update port

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,6 @@ import taskRouter from "./routes/task.route";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.MONGODB_URL as string)
   .then(() => {
@@ -47,6 +45,7 @@ app.use(
   }
 );
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
