e0fef7a79c0184fcb2889e93b5968366b2b98249
Pritam Ghosh <pri10145@gmail.com>
2024-07-30 08:35:15 +0530

    ...

--- clients/middleware.ts
+++ clients/middleware.ts
@@ -5,7 +5,8 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("trello_token");
   const url = request.nextUrl.clone();
 
-  // Redirect to sign-in if no token and not already on sign-in or sign-up
+  console.log(token)
+
   if (!token) {
     if (url.pathname !== "/sign-in" && url.pathname !== "/sign-up") {
       console.timeEnd("middleware");
@@ -13,7 +14,6 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(url);
     }
   } else {
-    // Redirect to home if token exists and user tries to access sign-in or sign-up
     if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
       console.timeEnd("middleware");
       url.pathname = "/";
@@ -21,11 +21,10 @@ export function middleware(request: NextRequest) {
     }
   }
 
-  // Continue to the requested page if all checks pass
   console.timeEnd("middleware");
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/sign-in", "/sign-up"], // Match all paths for better redirection
+  matcher: ["/", "/sign-in", "/sign-up"],
 };
