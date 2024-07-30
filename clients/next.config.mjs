c58ae790de0035782fa2354223cb26d8d79ee267
Pritam Ghosh <pri10145@gmail.com>
2024-07-30 09:06:50 +0530

    next config

--- clients/next.config.mjs
+++ clients/next.config.mjs
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {protocol: "https", hostname: "lh3.googleusercontent.com"}
-        ]
-    }
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_FRONTEND_URL || "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
